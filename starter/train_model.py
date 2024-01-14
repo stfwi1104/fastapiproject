7e7901791054d9eb09e2a85e887c0afca494b9e5
stfwi1104 <stfwi@web.de>
2024-01-14 19:01:41 +0100

    autopep8

--- starter/train_model.py
+++ starter/train_model.py
@@ -2,7 +2,7 @@
 
 from sklearn.model_selection import train_test_split
 from ml.data import process_data
-from ml.model import train_model 
+from ml.model import train_model
 from ml.model import compute_model_metrics
 from sklearn import tree
 import pandas as pd
@@ -19,14 +19,15 @@ data_file = "census.csv"
 
 
 # Remove whitespaces
-data = pd.read_csv(data_path+data_file)
+data = pd.read_csv(data_path + data_file)
 for column in data.columns:
-    data = data.rename(columns={column : column.strip()})
+    data = data.rename(columns={column: column.strip()})
 
 
 print(data.head())
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+# Optional enhancement, use K-fold cross validation instead of a
+# train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -45,9 +46,8 @@ X_train, y_train, encoder, lb = process_data(
 
 # Proces the test data with the process_data function.
 X_test, y_test, encoder, lb = process_data(
-   test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
 )
-
 
 
 # Train model.
@@ -60,7 +60,7 @@ pickle.dump(lb, open('../model/lb.pkl', 'wb'))
 
 # Calculate Model-Metrics
 preds = model.predict(X_test)
-precision, recall, fbeta = compute_model_metrics(y_test,preds)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
 print(precision)
 print(recall)
 print(fbeta)
@@ -71,12 +71,8 @@ for feature in cat_features:
     for cls in data[feature].unique():
         data_sclice = data[data[feature] == cls]
         X_slice, y_sclice, encoder, lb = process_data(
-        data_sclice, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
-        )        
+            data_sclice, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb)
         preds_sclice = model.predict(X_slice)
-        precision, recall, fbeta = compute_model_metrics(y_sclice,preds_sclice)
+        precision, recall, fbeta = compute_model_metrics(
+            y_sclice, preds_sclice)
         print(f"feature: {feature} value: {cls} precision: {precision}")
-      
-
-
-
